c73a28b562d4cedf68adb27fab6f105ef862a129
pavlov200912 <pavlov200912@gmail.com>
2020-05-04 13:02:45 +0300

    User interaction added

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -1,19 +1,39 @@
+import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import java.nio.file.NoSuchFileException
 
 object MainParser {
     @JvmStatic
     fun main(args: Array<String>) {
-        val charStream = CharStreams.fromFileName("./text.txt")
-        val lexer = GrammarLexer(charStream)
-        val commonTokenStream = CommonTokenStream(lexer)
-        val parser = GrammarParser(commonTokenStream)
+        while (true) {
+            println("Input filename with grammar:")
+            val line = readLine() ?: "exit"
+            if (line == "exit") {
+                println("Bye!")
+                break
+            }
+            val charStream: CharStream?
+            try {
+                charStream = CharStreams.fromFileName(line)
+            } catch (e: NoSuchFileException) {
+                println("No such file!")
+                continue
+            }
+            try {
+                val lexer = GrammarLexer(charStream)
+                val commonTokenStream = CommonTokenStream(lexer)
+                val parser = GrammarParser(commonTokenStream)
 
-        val parseTree = parser.my_rules();
-        val visitor = Visitor()
-        val ast = visitor.visit(parseTree)
-        ast.print()
-
+                val parseTree = parser.my_rules();
+                val visitor = Visitor()
+                val ast = visitor.visit(parseTree)
+                ast.print()
+                println()
+            } catch (e: ParseException) {
+                println("Parse error")
+            }
+        }
     }
 }
 
